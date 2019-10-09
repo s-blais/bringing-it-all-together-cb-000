c24c54258f8d4632f5118778a1feacfd08b53901
Stephen Blais <stephen@s-blais.com>
2019-10-09 19:14:54 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class dog
+
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def initialize (id: nil, name:, breed:)
+  end
+
+
+
+end
