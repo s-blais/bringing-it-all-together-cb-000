fb97a641b8eaeee3ea9f75b1399e87a5eb047d61
Stephen Blais <stephen@s-blais.com>
2019-10-09 20:43:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,7 @@ class Dog
     result = DB[:conn].execute(sql, name, breed)
     if !result.empty?
       dog_data = result[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed; dog_data[2])
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = dog.create(name: name, breed: breed)
     end
