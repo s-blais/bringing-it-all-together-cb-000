af4252224b9ea1063594a34a6fde55e2c81e660d
Stephen Blais <stephen@s-blais.com>
2019-10-09 20:45:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,7 @@ class Dog
       dog_data = result[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      dog = dog.create(name: name, breed: breed)
+      dog = Dog.create(name: name, breed: breed)
     end
     dog
   end
